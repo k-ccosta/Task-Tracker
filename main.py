a02c607112b055c26116d88e101101d0c3cca32b
k-ccosta <k.accosta98@gmail.com>
2025-01-29 10:43:18 -0400

    feat: code modularization

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-print("""
-        Task Tracker
-      """)
+def exibir_titulo():
+    print("""
+            Task Tracker
+        """)
 
-menu_opcoes = ["Adicionar Tarefa", "Atualizar Tarefa", "Excluir Tarefa"]
+def exibir_menu():
+    menu_opcoes = ["Adicionar Tarefa", "Atualizar Tarefa", "Excluir Tarefa"]
 
-for indice, opcao in enumerate(menu_opcoes):
-    print(f"[{indice}] - {opcao}")
+    for indice, opcao in enumerate(menu_opcoes):
+        print(f"[{indice}] - {opcao}")
 
+if __name__ == "__main__":
+    exibir_titulo()
+    exibir_menu()
