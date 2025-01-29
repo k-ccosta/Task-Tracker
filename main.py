9d96566281ee5ab95894aac1c01d44cf73a252bb
k-ccosta <k.accosta98@gmail.com>
2025-01-29 10:31:25 -0400

    initializing project

---
+++ main.py
@@ -0,0 +1,9 @@
+print("""
+        Task Tracker
+      """)
+
+menu_opcoes = ["Adicionar Tarefa", "Atualizar Tarefa", "Excluir Tarefa"]
+
+for indice, opcao in enumerate(menu_opcoes):
+    print(f"[{indice}] - {opcao}")
+
