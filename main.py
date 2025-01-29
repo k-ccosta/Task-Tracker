bd6e9aea457f5b8b5f4df6204cd531f8f00cdef2
k-ccosta <k.accosta98@gmail.com>
2025-01-29 11:07:22 -0400

    feat: adding function to clear the screen with the 'os' library

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+import os
+
+def limpar_tela():
+    os.system("cls" if os.name == "nt" else "clear")  
 def exibir_titulo():
     print("""
             Task Tracker
         """)
-
 def exibir_menu():
     menu_opcoes = ["Adicionar Tarefa", "Atualizar Tarefa", "Excluir Tarefa"]
 
     for indice, opcao in enumerate(menu_opcoes):
         print(f"[{indice}] - {opcao}")
 
+    return menu_opcoes
+
+def selecionar_menu(menu_opcoes):
+    while True:
+        try:
+            indice_selecionado = int(input("\nO que você deseja fazer? "))
+            
+            if indice_selecionado in range(len(menu_opcoes)):
+                break
+            else:
+                print(f"OPÇÃO INVÁLIDA! Por favor, selecione uma opção entre 0 e {len(menu_opcoes)-1}")                
+        except ValueError:
+            print("OPÇÃO INVÁLIDA! Por favor, informe um número inteiro.")
+
+
 if __name__ == "__main__":
     exibir_titulo()
-    exibir_menu()
+    menu_opcoes = exibir_menu()
+    selecionar_menu(menu_opcoes)
